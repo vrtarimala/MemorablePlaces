70ed135e06a090148a0c6803a12cd26ddeeaf530
vigneshreddy <vrtarimala@gmail.com>
2022-07-28 00:21:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/memorableplaces/MapsActivity.java
@@ -0,0 +1,136 @@
+package com.example.memorableplaces;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.memorableplaces.databinding.ActivityMapsBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    public void centerMapOnLocation(Location location,String title){
+        if(location!=null) {
+            LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+
+            mMap.addMarker(new MarkerOptions().position(userLocation).title(title));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 13));
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                Location lastKnownLocation=locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setOnMapLongClickListener(this);
+        Intent intent=getIntent();
+        if(intent.getIntExtra("placeNumber",0)==0){
+            // zoom in on userLocation
+            locationManager=(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            locationListener=new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    centerMapOnLocation(location,"Your Location");
+                }
+            };
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                Location lastKnownLocation=locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+                centerMapOnLocation(lastKnownLocation,"Your Location");
+            }else{
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+
+            }
+        }
+        else{
+            Location placeLocation=new Location(LocationManager.GPS_PROVIDER);
+            placeLocation.setLatitude(MainActivity.locations.get(intent.getIntExtra("placeNumber",0)).latitude);
+            placeLocation.setLongitude(MainActivity.locations.get(intent.getIntExtra("placeNumber",0)).longitude);
+            centerMapOnLocation(placeLocation,MainActivity.places.get(intent.getIntExtra("placeNumber",0)));
+        }
+
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        Geocoder geocoder=new Geocoder(getApplicationContext(), Locale.getDefault());
+        String address="";
+        try{
+            List<Address> listAddresses=geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            if(listAddresses!=null && listAddresses.size()>0){
+                if(listAddresses.get(0).getThoroughfare()!=null){
+                    if(listAddresses.get(0).getSubThoroughfare()!=null){
+                        address+=listAddresses.get(0).getSubThoroughfare()+" ";
+                    }
+                    address+=listAddresses.get(0).getThoroughfare();
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        if(address.equals("")){
+            SimpleDateFormat sdf=new SimpleDateFormat("HH:mm yyyy-MM-dd");
+            address+=sdf.format(new Date());
+        }
+        mMap.addMarker(new MarkerOptions().position(latLng).title(address));
+        MainActivity.places.add(address);
+        MainActivity.locations.add(latLng);
+        MainActivity.arrayAdapter.notifyDataSetChanged();
+        Toast.makeText(getApplicationContext(),"Location saved",Toast.LENGTH_SHORT).show();
+
+    }
+}
